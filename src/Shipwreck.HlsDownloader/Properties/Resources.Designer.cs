0bcff94297249e31bdada3975aa8715731a8303f
pgrho <shipwreck@live.jp>
2019-03-09 00:16:42 +0900

    Localize UI

--- src/Shipwreck.HlsDownloader/Properties/Resources.Designer.cs
+++ src/Shipwreck.HlsDownloader/Properties/Resources.Designer.cs
@@ -4,67 +4,158 @@
 //     ランタイム バージョン:4.0.30319.42000
 //
 //     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
-//     コードが再生成されるときに損失したりします
+//     コードが再生成されるときに損失したりします。
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Shipwreck.HlsDownloader.Properties
-{
-
-
+namespace Shipwreck.HlsDownloader.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   ローカライズされた文字列などを検索するための、厳密に型指定されたリソース クラスです。
     /// </summary>
-    // このクラスは StronglyTypedResourceBuilder クラスによって ResGen
+    // このクラスは StronglyTypedResourceBuilder クラスが ResGen
     // または Visual Studio のようなツールを使用して自動生成されました。
     // メンバーを追加または削除するには、.ResX ファイルを編集して、/str オプションと共に
-    // ResGen を実行し直すか、または VS プロジェクトをリビルドします。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    // ResGen を実行し直すか、または VS プロジェクトをビルドし直します。
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    public class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
-        ///   このクラスで使用されるキャッシュされた ResourceManager インスタンスを返します。
+        ///   このクラスで使用されているキャッシュされた ResourceManager インスタンスを返します。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Shipwreck.HlsDownloader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   すべてについて、現在のスレッドの CurrentUICulture プロパティをオーバーライドします
         ///   現在のスレッドの CurrentUICulture プロパティをオーバーライドします。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Size に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string ContentLength {
+            get {
+                return ResourceManager.GetString("ContentLength", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Format に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string ContentType {
+            get {
+                return ResourceManager.GetString("ContentType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Destination .m3u8 (_D) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string DestinationM3u8 {
+            get {
+                return ResourceManager.GetString("DestinationM3u8", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Save に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Download {
+            get {
+                return ResourceManager.GetString("Download", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Downloader (_E) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Downloader {
+            get {
+                return ResourceManager.GetString("Downloader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Download to Folder (_F) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string DownloadToFolder {
+            get {
+                return ResourceManager.GetString("DownloadToFolder", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Go に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string GoToUrl {
+            get {
+                return ResourceManager.GetString("GoToUrl", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Select に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string SelectM3u8 {
+            get {
+                return ResourceManager.GetString("SelectM3u8", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Target .m3u8 (_S) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string SourceM3u8 {
+            get {
+                return ResourceManager.GetString("SourceM3u8", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Status に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string StatusCode {
+            get {
+                return ResourceManager.GetString("StatusCode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Web View (_V) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string WebView {
+            get {
+                return ResourceManager.GetString("WebView", resourceCulture);
             }
         }
     }
